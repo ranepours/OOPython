b16a74cbb128e95bd243f58ed9c5f0607837bbe6
ranepours <ranejdavis@gmail.com>
2022-07-03 18:24:50 -0400

    OOOP

--- oop.py
+++ oop.py
@@ -1,5 +1,5 @@
-#serial number generator
 class SerialGenerator:
+    """serial number generator - create unique incrementing serials"""
     def __init__(self, start=0):
         self.start = self.next = start
 
@@ -13,3 +13,19 @@ class SerialGenerator:
     def reset(self):
         self.next = self.start
 
+import random
+class WordFinder:
+    """find random words from dictionary"""
+    def __init__(self, path):
+        dict_file = open(path)
+        self.words = self.parse(dict_file)
+        print(f"{len(self.words)} words read")
+    def parse(self, dict_file):
+        return [w.strip() for w in dict_file]
+    def random(self):
+        return random.choice(self.words)
+
+class SpecialwordFinder(WordFinder):
+    """finds random but excludes blank lines and comments"""
+    def parse(self, dict_file):
+        return [w.strip() for w in dict_file if w.strip() and not w.startswith('#')]
